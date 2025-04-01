b2b60e26b0c625ba0f4a46eb219bdbd0b5096b87
pedroChastalo <pedrochastalo@gmail.com>
2025-04-01 17:44:29 -0300

    Teste

--- NodeJS/modulo01/index.js
+++ NodeJS/modulo01/index.js
@@ -49,7 +49,7 @@ server.delete('/cursos/:index', (req, res) => {
 
     cursos.splice(index, 1);
 
-    return res.json({message: 'Curso deletado com sucehsso!'});
+    return res.json({message: 'Curso delddetado com sucehsso!'});
 })
 
 server.listen(3000);
